8f5b38123edde36afc338dd711defd8ecc7493f9
skichrome <campeoltoni@gmail.com>
2021-01-19 13:43:33 +0100

    refactor: Added comments to explain what I am doing

--- test-auto-changelog.ino
+++ test-auto-changelog.ino
@@ -1,14 +1,17 @@
 void setup()
 {
+  // Setup led and power it off
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void loop()
 {
+  // Power led on
   digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
 
+  // Power led off
   digitalWrite(LED_BUILTIN, HIGH);
   delay(1000);
 }
